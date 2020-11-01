14a2c83d106f29b313b67e441ad35031410182c5
KazakosVas <73130623+KazakosVas@users.noreply.github.com>
2020-11-01 17:06:40 +0200

    Add files via upload

---
+++ list.c
@@ -0,0 +1,147 @@
+//Authors Kazakos Vasileios , Farao Georgios , Manolis Stivaktas
+#include "list.h"
+
+list * new_list(void)
+{
+    list * mylist = malloc(sizeof( list));
+    mylist->start = NULL;
+    mylist->end = NULL;
+    
+    return mylist;
+}
+
+lnode * new_lnode(char * jsonName)
+{
+    lnode * node = malloc(sizeof(lnode));
+    node->json_name = malloc(strlen(jsonName) +1);
+    strcpy(node->json_name,jsonName);
+    
+    node->next = NULL;
+    
+    return node;
+}
+
+void insert_lnode( list* list,  lnode* lnode)
+{
+    if(list->start == NULL)
+    {
+        list->start = lnode;
+        list->end = lnode;
+    }
+    else
+    {
+        list->end->next = lnode;
+        list->end = lnode;
+    }
+}
+
+/*
+int delete_key_list(struct list * list, char * key) //returns 1 if key was found and deleted in the list, else 0
+{
+    if(strcmp(list->start->patient->entryDate, key) == 0) //if key is in list->start
+    {
+        
+        if(list->start == list->end) //if only one node in list
+            list->end = NULL;
+        
+        struct lnode *to_del = list->start;
+        list->start = list->start->next; //update list->start
+        deletePatientRecord(to_del->patient);
+        free(to_del);
+        
+        return 1;
+    }
+    else
+    {
+        struct lnode *temp = list->start;
+        
+        while(temp->next != NULL)
+        {
+            if(strcmp(temp->next->patient->entryDate, key) == 0)
+            {
+                if(list->end == temp->next) //if key in list->end update list->end
+                    list->end = temp;
+                
+                struct lnode *to_del = temp->next;
+                temp->next = temp->next->next; //update temp->next
+                deletePatientRecord(to_del->patient);
+                free(to_del);
+                
+                return 1;
+            }
+            else
+                temp = temp->next;
+        }
+        
+        return 0;
+    }
+    
+}
+*/
+void delete_list( list * list)
+{
+    if (list->start == NULL)
+    {
+        printf("List is empty (delete_list)\n");
+        return;
+    }
+    else
+    {
+         lnode *prev = list->start;
+        
+        while(list->start != NULL)
+        {
+            list->start = list->start->next;
+            free(prev->json_name);
+            free(prev);
+            prev = list->start;
+        }
+    }
+    
+    list->start = list->end = NULL;
+    
+}
+void print_list( list * list)
+{
+    if(list->start == NULL)
+    {
+        printf("List is empty (print_list)\n");
+        return;
+    }
+    else
+    {
+        lnode *temp = list->start;
+        
+        while(temp != NULL)
+        {
+            printf("%s \t",temp->json_name);
+            temp = temp->next;
+        }
+    }
+}
+
+/*
+patientRecord * find_patient(struct list * list, int recID)
+{
+    if (list->start == NULL)
+    {
+        //        printf("List is empty (find_patient)\n");
+        return NULL;
+    }
+    else
+    {
+        struct lnode *temp = list->start;
+        
+        while(temp != NULL)
+        {
+            if(temp->patient->recordID == recID)
+                return temp->patient;
+            else
+                temp = temp->next;
+        }
+    }
+    
+    return NULL;
+}
+*/
+
---
+++ main.c
@@ -0,0 +1,61 @@
+//Authors Kazakos Vasileios , Farao Georgios , Manolis Stivaktas
+
+#include <stdio.h>
+
+#include "list.h"
+#include "RBtree.h"
+
+#include <sys/types.h>
+#include <dirent.h>
+#include <unistd.h>
+#include <errno.h>
+
+
+
+int main(int argc, const char * argv[]) {
+    
+    
+    
+    DIR* Fd;
+    struct dirent* my_file;
+    char * name = malloc(1100);
+    
+    printf("%s\n",argv[1]);
+    Fd = opendir (argv[1]);
+    if(Fd == NULL )
+    {
+        perror("Didnt open Directory");
+        return 1;
+    }
+    while (( my_file = readdir(Fd)) != NULL)
+    {       //read directory and count subdirs
+        
+        if (strcmp(my_file->d_name, ".") != 0 && strcmp(my_file->d_name, "..") != 0)
+        {
+            DIR * in_directory;
+            struct dirent * file_json;
+            char *full_path=malloc(strlen((argv[1]))+strlen(my_file->d_name)+2);
+            sprintf(full_path,"%s/%s",argv[1],my_file->d_name);
+            full_path[strlen(full_path)]='\0';
+            in_directory = opendir(full_path);
+            if(in_directory == NULL)
+            {
+                perror("Didnt open inner directory");
+                return 1;
+                
+            }
+            while (( file_json = readdir(in_directory)) != NULL)
+            {
+                if (strcmp(file_json->d_name, ".") != 0 && strcmp(file_json->d_name, "..") != 0)
+                {
+                    sprintf(name, "%s//%s",my_file->d_name , file_json->d_name);
+                    name[ strlen(name) - 5 ] = '\0';
+                    
+                    
+                }
+            }
+        }
+        
+    }
+}
+
