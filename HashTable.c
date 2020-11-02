e4e5ee907a4077be3e7e3e9e4d63b2131d961e30
GeorgeFarao <41078921+GeorgeFarao@users.noreply.github.com>
2020-11-02 13:03:50 +0200

    Update HashTable.c
    added function match_same_products.

--- HashTable.c
+++ HashTable.c
@@ -52,4 +52,45 @@ void insert_Record (char * Record , HashTable * table)
     
 }
 
+void match_same_products(HashTable * table , char * spec_id1 , char * spec_id2 )
+{
+    
+    
+    int index1 = hash1(spec_id1, table->size);
+    int index2 = hash1 (spec_id2,  table->size );
+    
+    struct node * tree_node1;
+    struct node * tree_node2;
+    
+    
+    tree_node1 = find_key_RBtree(table->Trees[index1], spec_id1);
+    tree_node2 = find_key_RBtree(table->Trees[index2], spec_id2);
+    
+    if(tree_node1 ==NULL || tree_node2== NULL)
+    {
+        printf("Something went bad in match same products ");
+        return ;
+    }
+    
+    lnode * lnode_temp = tree_node2->list_same_jsons->start;
+    
+    tree_node1->list_same_jsons->end->next = tree_node2->list_same_jsons->start;
+    tree_node1->list_same_jsons->size += tree_node2->list_same_jsons->size;
+    
+    
+    while( lnode_temp!=NULL)
+    {
+        struct node * neighbour;
+
+        int neighbour_hash_index = hash1(lnode_temp->json_name, table->size);
+        
+        neighbour = find_key_RBtree( table->Trees[ neighbour_hash_index ],  lnode_temp->json_name );
+        neighbour->list_same_jsons = tree_node1->list_same_jsons;
+        
+        lnode_temp = lnode_temp->next;
+    }
+    
+    
+}
+
 
