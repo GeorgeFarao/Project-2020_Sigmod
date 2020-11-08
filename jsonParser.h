0ee0d7b66f2f80f83d4d4dd412f3579fa327a0ae
KazakosVas <73130623+KazakosVas@users.noreply.github.com>
2020-11-08 15:57:21 +0200

    Add files via upload
    MyJsonParser

---
+++ jsonParser.h
@@ -0,0 +1,27 @@
+#include "list.h"
+
+typedef struct json_node {
+    char * category;
+    list * values;
+    struct json_node * next;
+} json_node;
+
+typedef struct json_list {
+    json_node * start;
+    json_node * end;
+    int number_of_categories;
+} json_list;
+
+///////////////////// FUNCTIONS /////////////////////
+
+json_list * new_json_list (void);
+
+json_node * new_json_node (char * category);
+
+void add_category_value (json_list * jsonList, char * category, char * str_value);
+
+void insert_json_list_node (json_list * jsonList, json_node * node);
+
+void delete_json_list (json_list * jsonList);
+
+void print_json_list (json_list * jsonList);
