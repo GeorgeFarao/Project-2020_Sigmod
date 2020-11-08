f6a8ad342604d80e12ae574cc514888eae06cb54
KazakosVas <73130623+KazakosVas@users.noreply.github.com>
2020-11-08 16:01:39 +0200

    Update jsonParser.c
    A function that adds to the category node a value or creates a new category node if we havent found this category yet

--- jsonParser.c
+++ jsonParser.c
@@ -40,3 +40,40 @@ void insert_json_list_node (json_list * jsonList, json_node * node)
 
     jsonList->number_of_categories += 1;
 }
+
+
+
+void add_category_value (json_list * jsonList, char * category, char * str_value)
+{
+    json_node * temp_node = jsonList->start;
+    lnode * json_node_value = new_lnode(str_value);
+
+    if (temp_node == NULL) // No json_nodes in json_list case
+    {
+        temp_node = new_json_node(category);
+        insert_lnode(temp_node->values,  json_node_value);
+        jsonList->start = temp_node;
+        jsonList->end = temp_node;
+        jsonList->number_of_categories += 1;  
+    }
+    else
+    {
+        while (temp_node != NULL)
+        {
+            if (strcmp(temp_node->category, category) == 0) // Found category so insert value at json_node
+            {
+                insert_lnode(temp_node->values,  json_node_value);
+                return;
+            }
+
+            temp_node = temp_node->next;
+        }
+
+        // No category found so create category and add value
+
+        temp_node = new_json_node(category); // Initialize json_node
+        insert_lnode(temp_node->values,  json_node_value); // Insert value at json_node
+        insert_json_list_node (jsonList, temp_node); // Insert json_node at json_list
+    }
+}
+
