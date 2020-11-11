ec36b432235cdefdf468054bcdb0d76626e85595
Manolis Stivaktas <72800022+mansstiv@users.noreply.github.com>
2020-11-11 21:06:39 +0200

    Update main.c

--- main.c
+++ main.c
@@ -84,7 +84,7 @@ int main(int argc, char * argv[]) {
                     char *second_full_path=malloc(strlen(full_path)+strlen(file_json->d_name)+2);
                     sprintf(second_full_path,"%s/%s",full_path,file_json->d_name);
                     //printf("%s\n\n\n",second_full_path);
-                    json_list * jsonList=NULL; /*Parser(second_full_path);*/
+                    json_list * jsonList = Parser(second_full_path);
                    // print_json_list(jsonList);
                     insert_Record(name, Table,jsonList);
 
@@ -113,7 +113,6 @@ int main(int argc, char * argv[]) {
             break;
         }
         int real_value= atoi(value);
-<<<<<<< HEAD
         //printf("%s %s %d\n",str1,str2,real_value);
         if (real_value==1){
             match_same_products(Table,str1,str2);
@@ -121,19 +120,6 @@ int main(int argc, char * argv[]) {
     }
 
     print_commons(Table);
-=======
-        if(real_value ==1)
-            match_same_products(Table,str1,str2);
-
-        
-    }
-
-    print_commons(Table);
-
-
-
-
-
->>>>>>> 3d5689bdcfc018b01e47a819478636216f1d3840
+    
 
 }
