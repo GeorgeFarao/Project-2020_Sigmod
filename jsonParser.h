9c90f5ec43dc3feb9a8b6e677962e48258ebc4b8
GeorgeFarao <41078921+GeorgeFarao@users.noreply.github.com>
2020-11-12 21:14:52 +0200

    Update jsonParser.h

--- jsonParser.h
+++ jsonParser.h
@@ -1,33 +1,36 @@
-//Authors Kazakos Vasileios , Farao Georgios , Manolis Stivaktas
+/* Authors Kazakos Vasileios , Farao Georgios , Manolis Stivaktas */
 
 #include "list.h"
 
 typedef struct json_node {
-    char * category;
-    list * values;
+    char * category; /* Category name */
+    list * values; /* List of possible values - Values of a category can be more than one */
     struct json_node * next;
 } json_node;
 
+/* Main json_list that holds info of product */
+/* Number of nodes of the list are equal to the different number of categories of the product */
 typedef struct json_list {
     json_node * start;
     json_node * end;
-    int number_of_categories;
+    int number_of_categories; 
 } json_list;
 
-///////////////////// FUNCTIONS /////////////////////
 
-json_list * new_json_list (void);
+/////////////////////////////////////////   FUNCTIONS   /////////////////////////////////////////
 
-json_node * new_json_node (char * category);
+json_list * new_json_list (void); /* Creating main json list with product info */
 
-void add_category_value (json_list * jsonList, char * category, char * str_value);
+json_node * new_json_node (char * category); /* New json node - New category to product */
 
-void insert_json_list_node (json_list * jsonList, json_node * node);
+void insert_json_list_node (json_list * jsonList, json_node * node); /* Insert json node - category to json list */
 
-void delete_json_list (json_list * jsonList);
+void add_category_value (json_list * jsonList, char * category, char * str_value); /* Add value to category */
 
-void print_json_list (json_list * jsonList);
+void delete_json_list (json_list * jsonList); /* Deleting main json list */
+ 
+void print_json_list (json_list * jsonList); /* Print json list for debugging purposes */
 
-json_list * Parser(char *);
+int skip_whitespaces(FILE *); /* Skipping whitespaces */
 
-int skip_whitespaces(FILE *);
+json_list * Parser(char *); /* Parsing function that reads json file */
