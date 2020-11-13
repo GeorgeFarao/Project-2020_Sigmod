1d607b9fc6a2daeabefc8b28715861a45d19d89c
Manolis Stivaktas <72800022+mansstiv@users.noreply.github.com>
2020-11-13 19:49:59 +0200

    Added helpFunctions.h

---
+++ helpFunctions.h
@@ -0,0 +1,12 @@
+#include <sys/types.h>
+#include <dirent.h>
+#include <unistd.h>
+#include <errno.h>
+
+#include "HashTable.h"
+
+int get_max_size_line (char *file_name); /* Get maximum size of a line in file */ 
+
+void reading_directory (char * directory_name, HashTable * Table); /* Reading file and initializing structs */
+ 
+void reading_csv_file (char * file_name, HashTable * Table); /* Reading csv file and matching same products */ 
