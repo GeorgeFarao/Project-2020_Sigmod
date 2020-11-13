7c27fae69766810b9d4a481ad275f4a3c2d1150f
Manolis Stivaktas <72800022+mansstiv@users.noreply.github.com>
2020-11-13 17:29:11 +0200

    Update RBtree.h
    Added comments

--- RBtree.h
+++ RBtree.h
@@ -1,15 +1,15 @@
-//Authors Kazakos Vasileios , Farao Georgios , Manolis Stivaktas
+/* Authors Kazakos Vasileios , Farao Georgios , Manolis Stivaktas */
 
 #include <stdlib.h>
 #include <string.h>
 #include "jsonParser.h"
 
 struct node {
-    char color;  // for color property
-    struct node * left, * right, * parent; // links for left, right children and parent
-    char * key; // entryDate or id
-    list * list_same_jsons;
-    json_list * json_info;
+    char color;  /* for color property */
+    struct node * left, * right, * parent; /* links for left, right children and parent */
+    char * key; /* json file */
+    list * list_same_jsons; /* Pointer to list with matching json files of current json */
+    json_list * json_info; /* List of categories and values of each category of json file */
 };
 
 struct RBTree {
@@ -18,31 +18,38 @@ struct RBTree {
     char * directory_name;
 };
 
-///////////////////// FUNCTIONS //////////////////
+/////////////////////////////////////////   FUNCTIONS   /////////////////////////////////////////
 
-struct node * find_key_RBtree(struct RBTree *T, char *key);
+struct RBTree * new_RBTree(char * directory_name ); /* Creating new RedBlackTree */
 
-void postorder_print_commons(struct RBTree *T, struct node *node);
+struct node * new_node( char * json_id, json_list *); /* Creating new tree node */
 
+void LeftRotate(struct RBTree * T, struct node * x); /* Left rotate procedure */
 
-struct node * new_node( char * json_id, json_list *);
+void RightRotate(struct RBTree * T, struct node * y); /* Right rotate procedure */
 
-struct RBTree * new_RBTree(char * directory_name );
+/* RBTinsert calls RBTinsertFixup which secures Red Black Tree properties aren't violated */
+void RBTinsertFixup(struct RBTree * T, struct node * z); 
 
-void LeftRotate(struct RBTree * T, struct node * x);
+void RBTinsert(struct RBTree * T, struct node * z); /* Inseting node to RedBlackTree */
 
-void RightRotate(struct RBTree * T, struct node * y);
-
-void RBTinsertFixup(struct RBTree * T, struct node * z);
-
-void RBTinsert(struct RBTree * T, struct node * z);
-
+/* TRANSPLANT */
+/* node v is the brother of deleted node u */
+/* v will replace u */
+/* v is being updated */
 void transplant(struct RBTree * T, struct node * u, struct node * v);
 
+/* Returns node with minimum value in the subtree with root x */
 struct node * minimum(struct RBTree * T, struct node * x);
 
+/*  Traversing tree in postorder and deleting each node */
 void destroyRBTree(struct RBTree * T, struct node * node);
+
+/* Returns node with specidied key */
+/* If not found return NULL */
+struct node * find_key_RBtree(struct RBTree *T, char *key); 
+
+void postorder_print_commons(struct RBTree *T, struct node *node); /* Print matching json files */
 
 /* Check if height is balanced - Useful for unit testing */
 int isHeightBalanced(struct RBTree * tree, struct node *root, int *rootMax); 
-
