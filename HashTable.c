691bc319ba3fc00c8cfb254975ce2f3ab9f6b9ff
GeorgeFarao <41078921+GeorgeFarao@users.noreply.github.com>
2020-12-02 13:50:57 +0200

    Update HashTable.c

--- HashTable.c
+++ HashTable.c
@@ -47,9 +47,9 @@ void insert_Record (char * Record , HashTable * table, json_list * jsonList)
 
     }
     else            /* tree exits */
-        RBTinsert(table->Trees[ hash_index ], new_node(Record, jsonList));  /* We create and insert a node to the tree */
-
-
+    {
+        RBTinsert(table->Trees[hash_index],new_node(Record, jsonList) );  /* We create and insert a node to the tree */
+    }
 }
 
 
@@ -85,14 +85,14 @@ void match_different_products (HashTable * table , char * spec_id1 , char * spec
     newNode = new_node(lnode_temp->json_name, NULL);
     newNode->self_node=neighbour;
 
-
-
-
-    RBTinsert(tree_node1->list_same_jsons->different_cliques, newNode);
+    int res=-1;
+    res= RBTinsert(tree_node1->list_same_jsons->different_cliques, newNode);
+    if(res==0){
+        free(newNode->key);
+        free(newNode);
+    }
 
     lnode_temp = tree_node1->list_same_jsons->start;
-
-
 
 
     index_neig =hash1 (lnode_temp->json_name ,  table->size );
@@ -102,10 +102,12 @@ void match_different_products (HashTable * table , char * spec_id1 , char * spec
 
     newNode = new_node(lnode_temp->json_name, NULL);
     newNode->self_node=neighbour;
-    RBTinsert(tree_node2->list_same_jsons->different_cliques, newNode);
-
-
-
+     res=-1;
+    res= RBTinsert(tree_node2->list_same_jsons->different_cliques, newNode);
+    if(res==0){
+        free(newNode->key);
+        free(newNode);
+    }
 
 }
 
@@ -148,10 +150,9 @@ void match_same_products(HashTable * table , char * spec_id1 , char * spec_id2 )
                       tree_node2->list_same_jsons->different_cliques->root, tree_node1->list_same_jsons->start);
 
 
-
     combine_trees(tree_node2->list_same_jsons->different_cliques,
                   tree_node2->list_same_jsons->different_cliques->root,tree_node1->list_same_jsons->different_cliques);
-
+   // destroy_diffRBTree(tree_node2->list_same_jsons->different_cliques, tree_node2->list_same_jsons->different_cliques->root);
     /* Make first node's end point to the start of second node's list  and it's end to the second's end*/
     tree_node1->list_same_jsons->end->next = tree_node2->list_same_jsons->start;
     tree_node1->list_same_jsons->end = tree_node2->list_same_jsons->end;
@@ -159,36 +160,26 @@ void match_same_products(HashTable * table , char * spec_id1 , char * spec_id2 )
 
 
 
-
-
     /* We traverse second node's list and make each node in the list point to first's list */
     while( lnode_temp!=NULL)
     {
         struct node * neighbour;
-
         /* Find Every Node of the clique */
         int neighbour_hash_index = hash1(lnode_temp->json_name, table->size);
         neighbour = find_key_RBtree( table->Trees[ neighbour_hash_index ],  lnode_temp->json_name );
 
-        /* Only one node from the old list must delete it because they all point to same list item that contains 
+        /* Only one node from the old list must delete it because they all point to same list item that contains
 	and end information of the list   */
+
         if(strcmp(neighbour->list_same_jsons->end->json_name,neighbour->key)==0){
-            //destroy_diffRBTree(neighbour->list_same_jsons->different_cliques, neighbour->list_same_jsons->different_cliques->root);
+            destroy_diffRBTree(neighbour->list_same_jsons->different_cliques, neighbour->list_same_jsons->different_cliques->root);
             free(neighbour->list_same_jsons);
         }
-
-
 
         /* Make neighbour point to the new merged list */
         neighbour->list_same_jsons = tree_node1->list_same_jsons;
         lnode_temp = lnode_temp->next;
     }
-
-
-
-
-
-
 }
 
 
@@ -224,19 +215,17 @@ void delete_hashtable(HashTable * table)
 void combine_tree_list (HashTable * table, struct RBTree * Tree1, struct node * recursion_root, lnode * start)
 {
 
-    if(recursion_root == Tree1->NIL)
+    if(recursion_root == Tree1->NIL) {
+
         return;
+    }
     combine_tree_list(table, Tree1, recursion_root->left, start);
     combine_tree_list(table, Tree1, recursion_root->right, start);
-
-
 
 
     struct node * neighbour;
     int index_neig ;
     struct node * newNode;
-
-
 
 
     index_neig =hash1 (start->json_name ,  table->size );
@@ -246,8 +235,12 @@ void combine_tree_list (HashTable * table, struct RBTree * Tree1, struct node * 
 
     newNode =  new_node(start->json_name, NULL);
     newNode->self_node= neighbour;
-    RBTinsert( recursion_root->self_node->list_same_jsons->different_cliques , newNode);
-
+    int res=-1;
+    res=RBTinsert( recursion_root->self_node->list_same_jsons->different_cliques , newNode);
+    if(res==0){
+        free(newNode->key);
+        free(newNode);
+    }
 
 
 }
